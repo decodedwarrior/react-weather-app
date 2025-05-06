ea4ee3c8c444f27846d14a757bb1453e6369c083
Sumit <moharanasumit09@gmail.com>
2025-05-06 16:18:47 +0530

    PROJECT-3

---
+++ src/components/Weather.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Weather.css'
+import search from'../assets/search.jpg'
+import drizzel_icon from'../assets/drizzel.jpg'
+import humidi_icon from'../assets/humidi.png'
+import rain_icon from'../assets/rain.png'
+import wind_icon from'../assets/wind.png'
+import clear_icon from'../assets/clear.png'
+
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+        <div className="search-bar">
+            <input type="text" placeholder='search here'/>
+            <img src={search} alt=""/>
+        </div>
+        <img src={clear_icon} alt="" classsName="weather-icon"/>
+        <p className='temperature'>16°C</p>
+        <p className='location'>London</p>
+        <div className='weather-data'>
+            <div className="col">
+                <img src={humidi_icon} alt=""/>
+                <p>91%</p>
+                <span>Humidity</span>
+            </div>
+            <div className="col">
+                <img src={wind_icon} alt=""/>
+                <p>3.6 km/h</p>
+                <span>Wind Speed</span>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Weather
